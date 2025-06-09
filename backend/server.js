76d60e38eee3e99c8174c30dd89dbda77dd1aadd
anshryan <ansharyan9999@gmail.com>
2025-06-09 14:28:47 +0530

    fix image upload

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,8 @@ cloudinary.config({
 
 
 app.use(cors());
-app.use(express.json());
-app.use(express.json({limit: "5mb"}));
-app.use(express.urlencoded({ extended: true, limit: "5mb" }));
+app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 
 const __dirname = path.resolve();
